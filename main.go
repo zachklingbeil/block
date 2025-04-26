4e62fd03c1651044e2d0c90ebbffdb332cb34cfb
zachklingbeil <zach@zachklingbeil.com>
2025-04-25 20:59:31 -0700

    commit

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
+	"github.com/zachklingbeil/block/circuit"
 	"github.com/zachklingbeil/block/token"
 	"github.com/zachklingbeil/factory"
 )
 
 func main() {
 	factory := factory.Assemble("timefactory", 0)
-	// circuit := circuit.NewCircuit(factory)
+	circuit := circuit.NewCircuit(factory)
 
 	token.NewTokens(factory)
-
-	// circuit.Continue()
-
-	// factory.Json.Print(circuit.Get("zachklingbeil.eth"))
-	// factory.Json.Print(circuit.Get("35773"))
-	// factory.Json.Print(circuit.Get("eTh"))
-	// factory.Json.Print(circuit.Get("Lrc"))
-	// factory.Json.Print(circuit.Get("friction"))
-	// factory.Json.Print(circuit.Get(0))
-	// factory.Json.Print(circuit.Get(1))
-	// factory.Json.Print(circuit.Get("0")) // empty
-	// factory.Json.Print(circuit.Get("1"))
-	// factory.Json.Print(circuit.Get("30"))
-	// factory.Json.Print(circuit.Get(30))
+	circuit.Continue()
 
 	select {}
-	// loop := loopring.Connect(factory, circuit)
-	// loop.Loop()
-	// loop.BlockByBlock(55555)
-
 }
+
+// loop := loopring.Connect(factory, circuit)
+// loop.Loop()
+// loop.BlockByBlock(55555)
+
+// factory.Json.Print(circuit.Get("zachklingbeil.eth"))
+// factory.Json.Print(circuit.Get("35773"))
+// factory.Json.Print(circuit.Get("eTh"))
+// factory.Json.Print(circuit.Get("Lrc"))
+// factory.Json.Print(circuit.Get("friction"))
+// factory.Json.Print(circuit.Get(0))
+// factory.Json.Print(circuit.Get(1))
+// factory.Json.Print(circuit.Get("0")) // empty
+// factory.Json.Print(circuit.Get("1"))
+// factory.Json.Print(circuit.Get("30"))
+// factory.Json.Print(circuit.Get(30))
