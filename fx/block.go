1f972dcc0154edfdcb0e5d870a1bdb90050e69e1
zachklingbeil <zach@zachklingbeil.com>
2026-02-12 12:16:09 -0800

    simplify...

--- fx/block.go
+++ fx/block.go
@@ -10,16 +10,16 @@ import (
 )
 
 type Block struct {
-	Number       *big.Int    `json:"number"`
-	Hash         common.Hash `json:"hash"`
-	Timestamp    uint64      `json:"timestamp"`
-	GasLimit     uint64      `json:"gasLimit"`
-	GasUsed      uint64      `json:"gasUsed"`
-	BaseFee      *big.Int    `json:"baseFeePerGas,omitempty"`
-	Transactions []*Receipt  `json:"transactions"`
+	Number       *big.Int       `json:"number"`
+	Hash         common.Hash    `json:"hash"`
+	Timestamp    uint64         `json:"timestamp"`
+	GasLimit     uint64         `json:"gasLimit"`
+	GasUsed      uint64         `json:"gasUsed"`
+	BaseFee      *big.Int       `json:"baseFeePerGas,omitempty"`
+	Transactions []*Transaction `json:"transactions"`
 }
 
-type Receipt struct {
+type Transaction struct {
 	TxHash            common.Hash     `json:"hash"`
 	TxIndex           uint            `json:"index"`
 	From              common.Address  `json:"from"`
@@ -30,21 +30,13 @@ type Receipt struct {
 	Gas               uint64          `json:"gas"`
 	EffectiveGasPrice *big.Int        `json:"gasPrice"`
 	ContractAddress   *common.Address `json:"contractAddress,omitempty"`
-	Logs              []*Log          `json:"logs,omitempty"`
+	Logs              []*Event        `json:"logs,omitempty"`
 }
 
-type Log struct {
-	Address common.Address    `json:"address"`
-	Topics  *Topics           `json:"topics"`
-	Data    string            `json:"data,omitempty"`
-	Indexed map[string]string `json:"indexed,omitempty"`
-}
-
-type Topics struct {
-	Zero  string `json:"0"`
-	One   string `json:"1,omitempty"`
-	Two   string `json:"2,omitempty"`
-	Three string `json:"3,omitempty"`
+type Event struct {
+	Address common.Address `json:"contract"`
+	Topics  []string       `json:"topics"`
+	Data    string         `json:"data,omitempty"`
 }
 
 func (fx *Fx) Block(number *big.Int) (*Block, error) {
@@ -58,31 +50,30 @@ func (fx *Fx) Block(number *big.Int) (*Block, error) {
 		return nil, err
 	}
 
-	ethTxs := block.Transactions()
 	signer := types.MakeSigner(fx.Chain, block.Number(), block.Time())
-	txs := make([]*Receipt, len(ethTxs))
-	for i, tx := range ethTxs {
-		r := receipts[i]
+	txs := make([]*Transaction, len(block.Transactions()))
 
+	for i, tx := range block.Transactions() {
+		r := receipts[i]
 		from, _ := types.Sender(signer, tx)
 
-		var contractAddr *common.Address
+		var contract *common.Address
 		if r.ContractAddress != (common.Address{}) {
-			contractAddr = &r.ContractAddress
+			contract = &r.ContractAddress
 		}
 
-		txs[i] = &Receipt{
+		txs[i] = &Transaction{
 			TxHash:            tx.Hash(),
 			TxIndex:           uint(i),
 			From:              from,
 			To:                tx.To(),
 			Value:             tx.Value(),
-			Input:             "0x" + hex.EncodeToString(tx.Data()),
+			Input:             hexEncode(tx.Data()),
 			Status:            r.Status,
 			Gas:               r.GasUsed,
 			EffectiveGasPrice: r.EffectiveGasPrice,
-			ContractAddress:   contractAddr,
-			Logs:              fx.Logs(r.Logs),
+			ContractAddress:   contract,
+			Logs:              events(r.Logs),
 		}
 	}
 
@@ -109,64 +100,22 @@ func (fx *Fx) blockReceipts(number *big.Int) ([]*types.Receipt, error) {
 	return receipts, nil
 }
 
-func (fx *Fx) Logs(raw []*types.Log) []*Log {
-	// Collect unique contract addresses
-	addrSeen := make(map[common.Address]struct{})
-	var addrs []common.Address
-	for _, l := range raw {
-		if _, ok := addrSeen[l.Address]; !ok {
-			addrSeen[l.Address] = struct{}{}
-			addrs = append(addrs, l.Address)
-		}
-	}
-
-	// Build event ABI map per contract address
-	abiMap := make(map[common.Address]map[common.Hash]*EventABI)
-	for _, addr := range addrs {
-		abi, err := fx.ContractABI(addr)
-		if err != nil {
-			continue
-		}
-		abiMap[addr] = ParseEvents(abi)
-	}
-
-	logs := make([]*Log, len(raw))
+func events(raw []*types.Log) []*Event {
+	out := make([]*Event, len(raw))
 	for i, l := range raw {
-		t := &Topics{}
-		data := "0x" + hex.EncodeToString(l.Data)
-		var indexed map[string]string
-
-		if len(l.Topics) > 0 {
-			resolved := false
-
-			if events, ok := abiMap[l.Address]; ok {
-				if event, ok := events[l.Topics[0]]; ok {
-					resolved = true
-					t.Zero = event.Sig
-					indexed = DecodeIndexed(event, l.Topics)
-				}
-			}
-
-			if !resolved {
-				t.Zero = l.Topics[0].Hex()
-				if len(l.Topics) > 1 {
-					t.One = l.Topics[1].Hex()
-				}
-				if len(l.Topics) > 2 {
-					t.Two = l.Topics[2].Hex()
-				}
-				if len(l.Topics) > 3 {
-					t.Three = l.Topics[3].Hex()
-				}
-			}
+		topics := make([]string, len(l.Topics))
+		for j, t := range l.Topics {
+			topics[j] = t.Hex()
 		}
-
-		logs[i] = &Log{
+		out[i] = &Event{
 			Address: l.Address,
-			Topics:  t,
-			Indexed: indexed,
-			Data:    data,
+			Topics:  topics,
+			Data:    hexEncode(l.Data),
 		}
 	}
-	return logs
+	return out
+}
+
+func hexEncode(b []byte) string {
+	return "0x" + hex.EncodeToString(b)
 }
